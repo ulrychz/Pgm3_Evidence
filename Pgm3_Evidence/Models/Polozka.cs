cde71d6634439ac2d3367711faf2eed41f137288
ulrych <ulrych@hotmail.cz>
2024-04-18 19:31:39 +0200

    Přidejte soubory projektu.

---
+++ Pgm3_Evidence/Models/Polozka.cs
@@ -0,0 +1,21 @@
+﻿namespace Pgm3_Evidence.Models
+{
+    public class Polozka
+    {
+        public Polozka() 
+        {
+            Datum = DateOnly.FromDateTime(DateTime.Now);
+        }
+        public Polozka (DateOnly datum, double naklady, double vynosy)
+        {
+            Datum = datum;
+            Naklady = naklady;
+            Vynosy = vynosy;
+        }
+        public double Naklady { get; set; }
+        public double Vynosy { get; set; }
+        public DateOnly Datum { get; set; } 
+
+        public double Zisk => Vynosy - Naklady;
+    }
+}
---
+++ Pgm3_Evidence/Pages/EvidenceV1.razor.cs
@@ -0,0 +1,21 @@
+﻿using Pgm3_Evidence.Models;
+
+namespace Pgm3_Evidence.Pages
+{
+    public partial class EvidenceV1
+    {
+        public Polozka Polozka { get; set; } = new Polozka();
+
+        public List<Models.Polozka> Polozky { get; set; } = new List<Polozka>();
+        public void Pridat()
+        {
+            Polozky.Add(new Polozka(Polozka.Datum, Polozka.Naklady, Polozka.Vynosy));
+            
+            RDataGrid.RefreshDataAsync();
+
+        }
+
+    }
+
+
+}
